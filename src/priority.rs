3598d84931655a50bc4e9a2ee7bb0b2fa905788d
Jacob Gorm Hansen <jhansen@dropbox.com>
2026-03-18 14:48:37 +0100

    set macos background priority to BACKGROUND not UTILITY

--- src/priority.rs
+++ src/priority.rs
@@ -3,7 +3,7 @@
 //! On Windows, sets thread priority to IDLE for the lifetime of the PriorityManager,
 //! restoring to NORMAL on drop.
 //!
-//! On macOS, sets QoS (Quality of Service) class to UTILITY for long-running work,
+//! On macOS, sets QoS (Quality of Service) class to BACKGROUND for long-running work,
 //! restoring the original QoS on drop.
 //!
 //! This makes long-running operations less intrusive to user-facing tasks.
@@ -46,14 +46,6 @@ mod platform_impl {
     use std::os::raw::{c_int, c_uint};
 
     // Darwin QoS (Quality of Service) classes
-    #[allow(dead_code)]
-    const QOS_CLASS_USER_INTERACTIVE: c_uint = 0x21;
-    #[allow(dead_code)]
-    const QOS_CLASS_USER_INITIATED: c_uint = 0x19;
-    #[allow(dead_code)]
-    const QOS_CLASS_DEFAULT: c_uint = 0x15;
-    const QOS_CLASS_UTILITY: c_uint = 0x11;
-    #[allow(dead_code)]
     const QOS_CLASS_BACKGROUND: c_uint = 0x09;
     const QOS_CLASS_UNSPECIFIED: c_uint = 0x00;
 
@@ -85,8 +77,8 @@ mod platform_impl {
                 let thread = pthread_self();
                 let _ = pthread_get_qos_class_np(thread, &mut original_qos, &mut original_priority);
 
-                // Set to UTILITY class for long-running work
-                let _ = pthread_set_qos_class_self_np(QOS_CLASS_UTILITY, 0);
+                // Set to BACKGROUND class for long-running work
+                let _ = pthread_set_qos_class_self_np(QOS_CLASS_BACKGROUND, 0);
 
                 Self {
                     original_qos,
